dc94b27541b6fd0b682b6006a2046ee838c2f764
Justin Johnson <bbohes@gmail.com>
2021-02-06 14:12:40 -0500

    we are back on track

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ export { default as UserPosts } from './UserPosts';
 export {default as Login} from './Login';
 export {default as SignUp} from './SignUp';
 export {default as SignOut} from './SignOut';
-export {default as Multimodal} from './Multimodal';
 export {default as Footer} from './Footer';
 export {default as CreatePost} from './CreatePost';
 export {default as MyPosts} from './MyPosts';
--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ import {
     Login, 
     SignUp,
     SignOut,
-    Multimodal,
     Footer,
     CreatePost,
     MyPosts
